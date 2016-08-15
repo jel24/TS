62a972082b435e34aba44503039299ba5ef0465a
DOMINGO\Jake <jake@jakelandry.com>
2016-08-14 23:47:46 -0700

    8/14 added doors and new area

---
+++ Assets/Scripts/AreaDoor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AreaDoor : MonoBehaviour {
+
+	public AreaDoor target;
+	public Camera camera;
+
+	void OnTriggerEnter (Collider collider)
+	{
+
+		if (collider.tag == "Player") { // player moving through door
+			Player player = collider.GetComponent<Player> (); // player entering or leaving
+
+			if (player.entering) { // move into current room
+				player.transform.Translate (Vector3.forward * 5);
+				player.entering = false;
+			} else { // move to another room
+				player.entering = true;
+				player.transform.position = target.transform.position;
+				player.SwitchCamera(target.camera);
+			}
+
+
+		}
+
+	}
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+
+	public bool entering = false;
+	private Camera activeCam;
+
+	// Use this for initialization
+	void Start () {
+		activeCam = FindObjectOfType<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SwitchCamera (Camera newCam)
+	{
+
+		if (isLocalPlayer) {
+
+			Debug.Log("Current camera is " + activeCam);
+			Debug.Log("Switching cameras to " + newCam.name);
+			newCam.gameObject.SetActive(true);
+			activeCam.gameObject.SetActive(false);
+			activeCam = newCam;
+
+
+
+
+
+
+		}
+
+	}
+}
